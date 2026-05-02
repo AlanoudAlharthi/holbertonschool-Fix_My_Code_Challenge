e5704f624f55a8bc3ee493cb42af44a746f5917c
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2026-05-02 19:19:32 +0000

    fix delete_dnodeint_at_index

---
+++ challenge/delete_dnodeint_at_index.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stdlib.h>
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *temp = *head;
+    unsigned int i = 0;
+
+    if (head == NULL || *head == NULL)
+        return (-1);
+
+    if (index == 0)
+    {
+        *head = temp->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+        free(temp);
+        return (1);
+    }
+
+    while (temp != NULL && i < index)
+    {
+        temp = temp->next;
+        i++;
+    }
+
+    if (temp == NULL)
+        return (-1);
+
+    if (temp->prev)
+        temp->prev->next = temp->next;
+
+    if (temp->next)
+        temp->next->prev = temp->prev;
+
+    free(temp);
+    return (1);
+}
