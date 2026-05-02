6aaa0f35e87f550146b61aaf945b08e6a3a72983
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2026-05-02 19:27:38 +0000

    fix delete_dnodeint

--- challenge/delete_dnodeint_at_index.c
+++ challenge/delete_dnodeint_at_index.c
@@ -3,12 +3,15 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *temp = *head;
-    unsigned int i = 0;
+    dlistint_t *temp;
+    unsigned int i;
 
     if (head == NULL || *head == NULL)
         return (-1);
 
+    temp = *head;
+
+    /* حذف أول عنصر */
     if (index == 0)
     {
         *head = temp->next;
@@ -18,19 +21,23 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
         return (1);
     }
 
+    /* الوصول للعنصر */
+    i = 0;
     while (temp != NULL && i < index)
     {
         temp = temp->next;
         i++;
     }
 
+    /* إذا ما وصلنا */
     if (temp == NULL)
         return (-1);
 
-    if (temp->prev)
+    /* إعادة الربط */
+    if (temp->prev != NULL)
         temp->prev->next = temp->next;
 
-    if (temp->next)
+    if (temp->next != NULL)
         temp->next->prev = temp->prev;
 
     free(temp);
